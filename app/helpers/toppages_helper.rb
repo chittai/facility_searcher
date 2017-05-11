e585a53f6aabe6a10a48c4b06fe2e7623c5af557
taquan <chittai1221@gmail.com>
2017-05-11 16:08:55 +0000

    modify toppage helper

--- app/helpers/toppages_helper.rb
+++ app/helpers/toppages_helper.rb
@@ -3,7 +3,8 @@ module ToppagesHelper
 
     search_key = keyword
 
-    @types_array = ['convenience_store','train_station','hospital']
+    @types_array = [params[:facility_first],params[:facility_second],params[:facility_third]]
+    #@types_array = ['convenience_store','train_station','hospital']
     
     @types = 'convenience_store'
     @types_second = 'train_station'
@@ -61,5 +62,55 @@ module ToppagesHelper
       
       @hash << { lat: @lat_search, lng: @lng_search, infowindow: "", title: ""}
     end  
+    
+    def type_searcher(type)
+      @types_list = {'convenience_store' => 'コンビニエンスストア', 'train_station' => '駅', 'hospital' => '病院'}
+      return @types_list[type]
+    end
+      
+    def calScore(distance)
+      @facility_score = 0
+      
+      if 0 <= distance && distance < 100
+        @facility_score = 100
+      elsif 100 <= distance && distance < 200 
+        @facility_score = 90
+      elsif 200 <= distance && distance < 300
+        @facility_score = 80
+      elsif 300 <= distance && distance < 400
+        @facility_score = 70
+      elsif 400 <= distance && distance < 500
+        @facility_score = 60
+      elsif 500 <= distance && distance < 600
+        @facility_score = 50
+      elsif 600 <= distance && distance < 700
+        @facility_score = 40
+      elsif 700 <= distance && distance < 800
+        @facility_score = 30
+      elsif 800 <= distance && distance < 900
+        @facility_score = 20
+      elsif 900 <= distance && distance < 1000
+        @facility_score = 10
+      else
+        @facility_score = 0
+      end
+    end
+    
+    
+    def calCoefficien(index)
+      
+      @coefficient = 0
+      
+      case index
+      when 0 then
+        @coefficient = 5
+      when 1 then
+        @coefficient = 2
+      when 2 then
+        @coefficient = 1
+      else
+      end
+    end
+      
   end
 end
